99f35ba992c886902bc14cdd3b0a1473c0954ef9
sadiecase29 <167736843+sadiecase29@users.noreply.github.com>
2024-06-08 12:03:30 -0700

    Create p1-date.js

---
+++ p1-date.js
@@ -0,0 +1,6 @@
+/*
+    CIT 281 Project 1
+    Name: Sadie Case
+*/
+const daysOfweek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; 
+console.log(daysOfweek[new Date().getDay()]);
